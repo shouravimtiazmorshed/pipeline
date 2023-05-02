df3ccc45efe9d8eb7d7a07b754c715ecb80390af
shouravimtiazmorshed <bumblebee.injoker@gmail.com>
2023-05-03 03:20:39 +0600

    CodeQL 33

---
+++ src/main/java/com/test/error/Error.java
@@ -0,0 +1,10 @@
+package com.test.error;
+
+public class Error {
+
+	String password ="ABC";
+	public void printPass()
+	{
+		System.out.println(password);;
+	}
+}
--- src/main/java/com/test/pipeline/PipelineApplication.java
+++ src/main/java/com/test/pipeline/PipelineApplication.java
@@ -3,11 +3,15 @@ package com.test.pipeline;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.test.error.Error;
+
 @SpringBootApplication
 public class PipelineApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PipelineApplication.class, args);
+		Error e = new Error();
+		e.printPass();
 	}
 
 }
